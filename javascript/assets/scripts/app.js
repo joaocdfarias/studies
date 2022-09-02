262adaba20de3adb55ba11ebcbba3f4c08c125f5
João Carlos de Farias Almeida <joaocdfariass@gmail.com>
2022-09-02 20:26:14 -0300

    feat: using variables

--- javascript/assets/scripts/app.js
+++ javascript/assets/scripts/app.js
@@ -1,1 +1,12 @@
-alert('This works!')
+let currentResult = 0
+
+currentResult = currentResult + 10
+console.log('1', currentResult)
+// In this case you assigning the previous value + 10
+// So currentResult = 0 + 10 = 10
+// If we do this again currentResult = 10 + 10 = 20
+currentResult = currentResult + 10
+console.log('2', currentResult)
+// Basically, currentResult is overwritten by itself + 10
+
+outputResult(currentResult, '')
