865cc65b0332db42d7b70048ab33663954dbb9b5
João Carlos de Farias Almeida <joaocdfariass@gmail.com>
2022-09-03 23:13:21 -0300

    refactor: use template strings

--- javascript/assets/scripts/app.js
+++ javascript/assets/scripts/app.js
@@ -5,6 +5,6 @@ let currentResult = defaultResult
 
 currentResult = ((currentResult + 10) * 3) / 2 - 1
 
-let calculationDescription = '(0 + 10) * 3 / 2 - 1'
+let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`
 
 outputResult(currentResult, calculationDescription)
