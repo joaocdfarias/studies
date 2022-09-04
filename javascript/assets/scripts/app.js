685a8f839166af77ec3c13a2860fcc2e7a18beda
João Carlos de Farias Almeida <joaocdfariass@gmail.com>
2022-09-03 22:55:41 -0300

    feat: second argument as conditional

--- javascript/assets/scripts/app.js
+++ javascript/assets/scripts/app.js
@@ -1,12 +1,7 @@
 let currentResult = 0
 
-currentResult = currentResult + 10
-console.log('1', currentResult)
-// In this case you assigning the previous value + 10
-// So currentResult = 0 + 10 = 10
-// If we do this again currentResult = 10 + 10 = 20
-currentResult = currentResult + 10
-console.log('2', currentResult)
-// Basically, currentResult is overwritten by itself + 10
+currentResult = ((currentResult + 10) * 3) / 2 - 1
 
-outputResult(currentResult, '')
+let calculationDescription = '(0 + 10) * 3 / 2 - 1'
+
+outputResult(currentResult, calculationDescription)
