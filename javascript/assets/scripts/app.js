25ad39d2070d7d487c53e367b5b2d84d6b8d085a
João Carlos de Farias Almeida <joaocdfariass@gmail.com>
2022-09-04 00:41:35 -0300

    refactor: pull function to bottom

--- javascript/assets/scripts/app.js
+++ javascript/assets/scripts/app.js
@@ -1,13 +1,14 @@
 const defaultResult = 0
 let currentResult = defaultResult
 
-function add(number1, number2) {
-  const result = number1 + number2
-  return result
-}
-
 currentResult = add(10, 4)
 
 let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`
 
 outputResult(currentResult, calculationDescription)
+
+// I can move the function to the bottom because JS pull functions to the top of the code
+function add(number1, number2) {
+  const result = number1 + number2
+  return result
+}
