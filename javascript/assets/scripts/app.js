9cd2377c0b651bd43893cd5ebeb09f80017a93eb
João Carlos de Farias Almeida <joaocdfariass@gmail.com>
2022-09-04 23:03:49 -0300

    fix: transform userInput.value into number

--- javascript/assets/scripts/app.js
+++ javascript/assets/scripts/app.js
@@ -1,14 +1,9 @@
 const defaultResult = 0
 let currentResult = defaultResult
 
-currentResult = add(10, 4)
-
-let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`
-
-outputResult(currentResult, calculationDescription)
-
-// I can move the function to the bottom because JS pull functions to the top of the code
-function add(number1, number2) {
-  const result = number1 + number2
-  return result
+function add() {
+  currentResult = currentResult + Number(userInput.value)
+  outputResult(currentResult, '')
 }
+
+addBtn.addEventListener('click', add)
