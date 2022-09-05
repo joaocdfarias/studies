0b800c570af886029c40de2b9fb0537644124736
João Carlos de Farias Almeida <joaocdfariass@gmail.com>
2022-09-04 23:08:29 -0300

    feat: adding functions

--- javascript/assets/scripts/app.js
+++ javascript/assets/scripts/app.js
@@ -1,9 +1,15 @@
 const defaultResult = 0
 let currentResult = defaultResult
 
+function getUserNumberInput() {
+  return Number(userInput.value)
+}
+
 function add() {
-  currentResult = currentResult + Number(userInput.value)
-  outputResult(currentResult, '')
+  const enteredNumber = getUserNumberInput()
+  const calcDescription = `${currentResult} + ${enteredNumber}`
+  currentResult = currentResult + enteredNumber
+  outputResult(currentResult, calcDescription)
 }
 
 addBtn.addEventListener('click', add)
