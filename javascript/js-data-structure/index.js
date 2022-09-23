eb19a53693ce264685a3bd9715f802c58e0ab3f6
João Carlos de Farias Almeida <joaocdfariass@gmail.com>
2022-09-22 23:19:14 -0300

    feat: @@iterator

--- javascript/js-data-structure/index.js
+++ javascript/js-data-structure/index.js
@@ -11,3 +11,8 @@ numbers.forEach((x) => console.log(isEven(x)));
 for (const number of numbers) {
   console.log(number % 2 === 0 ? 'even' : 'odd');
 }
+
+let iterator = numbers[Symbol.iterator]();
+console.log(iterator.next().value);
+console.log(iterator.next().value);
+console.log(iterator.next().value);
